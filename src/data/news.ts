5da92747fda3e001846145dd4332f57f22bc3861
developerleonardo <leo.11hnz@gmail.com>
2025-05-08 20:25:32 -0500

    feat: add image support to news items and update LastNews component

--- src/data/news.ts
+++ src/data/news.ts
@@ -6,17 +6,20 @@ export const news: Array<newsType> = [
     description:
       "A game development studio creating projects that primarily we would play ourselves first, and then aim at massive audiences. A game development studio creating projects that primarily we would",
     link: "/",
+    image: "/images/news-img-1.webp",
   },
   {
     title: "NEW 2",
     description:
       "A game development studio creating projects that primarily we would play ourselves first, and then aim at massive audiences. A game development studio creating projects that primarily we would",
     link: "/",
+    image: "/images/news-img-2.webp",
   },
   {
     title: "NEW 3",
     description:
       "A game development studio creating projects that primarily we would play ourselves first, and then aim at massive audiences. A game development studio creating projects that primarily we would",
     link: "/",
+    image: "/images/news-img-3.webp",
   },
 ];
