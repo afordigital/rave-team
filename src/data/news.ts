a22f8674f27567029776d9c76ee6857bffce6181
Juan Bautista <jbautistapulido@gmail.com>
2025-05-09 11:39:40 -0500

    LastNewsPage has been added, along with the Categories component, the categories section has been added for Filtering

--- src/data/news.ts
+++ src/data/news.ts
@@ -6,6 +6,7 @@ export const news: Array<newsType> = [
     description:
       "A game development studio creating projects that primarily we would play ourselves first, and then aim at massive audiences. A game development studio creating projects that primarily we would",
     link: "/",
+    category: "Crodfounding",
     image: "/images/news-img-1.webp",
   },
   {
@@ -13,6 +14,7 @@ export const news: Array<newsType> = [
     description:
       "A game development studio creating projects that primarily we would play ourselves first, and then aim at massive audiences. A game development studio creating projects that primarily we would",
     link: "/",
+    category: "Games",
     image: "/images/news-img-2.webp",
   },
   {
@@ -20,6 +22,29 @@ export const news: Array<newsType> = [
     description:
       "A game development studio creating projects that primarily we would play ourselves first, and then aim at massive audiences. A game development studio creating projects that primarily we would",
     link: "/",
+    category: "Events",
+    image: "/images/news-img-3.webp",
+  },
+  {
+    title: "NEW 4",
+    description: "Biggest crowdfund milestone achieved!",
+    link: "/",
+    category: "Crodfounding",
+    image: "/images/news-img-1.webp",
+  },
+  {
+    title: "NEW 5",
+    description: "Behind the scenes of our new game engine.",
+    link: "/",
+    category: "Games",
+    image: "/images/news-img-2.webp",
+
+  },
+  {
+    title: "NEW 6",
+    description: "We’re speaking at the Global Game Expo 2025.",
+    link: "/",
+    category: "Events",
     image: "/images/news-img-3.webp",
   },
 ];
